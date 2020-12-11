874da83dd906b6d6a8ecf9c9f504780c6f87d738
Josh Wiebe <jwiebe@gms.ca>
2020-12-11 11:54:31 -0600

    Created new dotnet console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kafka_consumer_sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
